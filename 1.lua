bc0e85b9b57fe5a03a3442186c58de35ff570f5e
lxmyysd <3253549307@qq.com>
2025-08-01 17:04:19 +0800

    Add files via upload

---
+++ 1.lua
@@ -0,0 +1,9 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/JsYb666/UI/refs/heads/main/%E5%BD%A9%E8%99%B9.lua))()"
+
+local window = library:new("矢井凛")
+
+local krx = window:Tab("矢井凛脚本",'Image_1753599569040')
+
+local about = krx:section("通用",true)
+
+
